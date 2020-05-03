f7a3aca2c2d36037df1664002bf7c92f7f63ef12
srgarcia <srgarcia@enhanceit.com>
2020-05-02 22:25:57 -0400

    Analytics example

---
+++ app/src/main/java/com/example/firebaseexample/analytics/ImageInfo.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseexample.analytics
+
+/**
+ * Pair of resource IDs representing an image and its title.
+ */
+data class ImageInfo(val image: Int, val title: Int, val id: Int)
